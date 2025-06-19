e203bab5c0094cf8d85ff8719c91942f0c485fc7
p28Gamaliel <gamaliel13perez@gmail.com>
2025-06-19 18:16:56 +0000

    c7

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - Imprime los tamaños de varios tipos en bytes
- * Return: 0 si el programa se ejecuta correctamente
+ * main - Prints the size of various types in bytes
+ * Return: 0 if the program executes successfully
  */
 	int main(void)
-{
-	printf("El tamaño de un char: %zu byte(s)\n", sizeof(char));
-	printf("El tamaño de un int: %zu byte(s)\n", sizeof(int));
-	printf("El tamaño de un long int: %zu byte(s)\n", sizeof(long int));
-	printf("El tamaño de un long long int: %zu byte(s)\n", sizeof(long long int));
-	printf("El tamaño de un float: %zu byte(s)\n", sizeof(float));
-	printf("El tamaño de un double: %zu byte(s)\n", sizeof(double));
+	{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	printf("Size of a double: %zu byte(s)\n", sizeof(double));
 
 	return (0);
-}
+	}
