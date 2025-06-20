22b1cda98cee7ea8b7c6c2f4ccb13de8a7206991
p28Gamaliel <gamaliel13perez@gmail.com>
2025-06-20 16:55:59 +0000

    cv5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Punto de entrada
+ *
+ * Return: Siempre 0 (exito)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d",n);
+	printf("\n");
+
+	return (0);
+}
