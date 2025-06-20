ab6707e6b1304ecd3fa16af1753e9109f24fc48b
p28Gamaliel <gamaliel13perez@gmail.com>
2025-06-20 13:51:27 +0000

    cv0

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Asigna un numero aleatorio a la variable n,
+ * 	luego imprime si el nÃmero es positivo, cero o negativo.
+ * Return: Siempre 0 (exito)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d es positivo\n", n);
+	else if (n == 0)
+		printf("%d es cero\n", n);
+	else
+		printf("%d es negativo\n", n);
+
+	return 0;
+}
