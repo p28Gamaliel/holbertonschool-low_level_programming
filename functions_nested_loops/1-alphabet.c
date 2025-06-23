acc68e471e3f1bf6d1ff3918f251b9a7de1f5cf2
p28Gamaliel <gamaliel13perez@gmail.com>
2025-06-23 15:05:55 +0000

    f1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - imprime el alfabeto en minusculas, seguido de una nueva linea
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char letra;
+
+	for (letra = 'a'; letra <= 'z'; letra++)
+	{
+		_putchar(letra);
+	}
+	_putchar('\n');
+}
