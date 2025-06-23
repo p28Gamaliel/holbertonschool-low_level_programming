52c9195216c9c454cbb7aa2539c0e0e1c95ce236
p28Gamaliel <gamaliel13perez@gmail.com>
2025-06-23 16:38:29 +0000

    f2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - imprime el alfabeto en minusculas 10 veces
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char letra;
+
+	for (i = 0; i < 10; i++) /*bucle externo: 10 veces*/
+	{
+		for (letra = 'a'; letra <= 'z'; letra++) /*bucle interno: a-z*/
+		{
+			_putchar(letra);
+		}
+		_putchar('\n'); /*salto de linea despues de cada linea de abecedario*/
+	}
+}
