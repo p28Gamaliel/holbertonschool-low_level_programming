690da3ebae8a15f1fbc3de358c38b43670e9a7ea
p28Gamaliel <gamaliel13perez@gmail.com>
2025-06-24 18:54:43 +0000

    f11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - imprime todos los números naturales desde n hasta 98
+ * @n: número de inicio
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+		}
+	}
+	printf("\n");
+}
