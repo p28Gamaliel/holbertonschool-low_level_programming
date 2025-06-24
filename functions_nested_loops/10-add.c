cc4be47a58e8f025e7d869a43087081c21e1efaf
p28Gamaliel <gamaliel13perez@gmail.com>
2025-06-24 18:09:54 +0000

    f10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - suma dos numeros
+ *
+ * @a: primer numero
+ * @b: segundo numero
+ *
+ * Return: La suma de esos dos numeros.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
