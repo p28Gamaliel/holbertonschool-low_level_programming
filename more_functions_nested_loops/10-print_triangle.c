c09459ee53d674a2ef51babd47c285a1aebb11ee
p28Gamaliel <gamaliel13perez@gmail.com>
2025-06-27 17:26:14 +0000

    f9

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_triangle - imptimr un triangulo luego de un salto de linea
+ *
+ * @size: variable a tomar el valor
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size - i; j++)
+				_putchar(' ');
+
+			for (k = 1; k <= i; k++)
+				_putchar('#');
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+}
