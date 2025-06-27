c1fe06841a830428f1c485be2e5aa317f07cad4f
p28Gamaliel <gamaliel13perez@gmail.com>
2025-06-27 16:07:22 +0000

    f7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - imprime una linea '\' en la terminal
+ *
+ * @n: numero de lineas que forman la diagonal
+ * 
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
