49502cf775d5ea6ab34531fa0b079a21ff1295fc
p28Gamaliel <gamaliel13perez@gmail.com>
2025-06-27 16:20:44 +0000

    f8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - imprime una linea '#' en la terminal
+ *
+ * @size: numero de lineas que forman el cuadrado
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
