fb5f95081b60c0f3adf7fabaa3ff389e23f96b81
p28Gamaliel <gamaliel13perez@gmail.com>
2025-07-02 14:50:55 +0000

    puts2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - imprime una cadena de numeros pares de 0 a 8
+ *
+ * @str: puntero
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
