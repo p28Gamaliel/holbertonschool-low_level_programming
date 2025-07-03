c9855fbfbd89566267fa5f811d31c791bc263497
p28Gamaliel <gamaliel13perez@gmail.com>
2025-07-03 17:58:07 +0000

    100-atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * _atoi - convierte una cadena a numero
@@ -16,21 +17,25 @@ int _atoi(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == '-' && !encontre)
-		{
-
+		if (s[i] == '-')
 			signo *= -1;
-		}
+		else if (s[i] == '+')
+			continue;
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
+			if (num > (INT_MAX - (s[i] - '0')) / 10)
+			{
+				if (signo == 1)
+					return INT_MAX;
+				else
+					return INT_MIN;
+			}
+
 			num = num * 10 + (s[i] - '0');
 			encontre = 1;
-
 		}
 		else if (encontre)
-		{
 			break;
-		}
 	}
 
 	return (num * signo);
