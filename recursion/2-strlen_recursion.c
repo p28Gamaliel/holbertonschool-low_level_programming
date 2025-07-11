0f60e78d6ee3c9726da050966a93062c28bd4cbd
p28Gamaliel <gamaliel13perez@gmail.com>
2025-07-11 19:13:14 +0000

    strlen

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -5,10 +5,11 @@
  *
  * @s: ubicacion de la cadena
  *
+ * Return: si no s no encuentra nada devuelve 0
  */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
-	return (1 + _strlen_recursion(s +1));
+	return (1 + _strlen_recursion(s + 1));
 }
