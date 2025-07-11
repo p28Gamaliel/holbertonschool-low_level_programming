75b472239cd26f2e60fb272651f05556850bb983
p28Gamaliel <gamaliel13perez@gmail.com>
2025-07-11 17:15:58 +0000

    print_diagsums

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,16 +1,16 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
  * print_diagsums - imprime la diagonal de una matriz
  *
- * @a: puntero donde buscara los valores
- * @size: el tama√o de la matriz cuadrada
+ * @a: puntero a la matriz
+ * @size: el tamano de la matriz cuadrada
  */
 void print_diagsums(int *a, int size)
 {
 	int i;
-	int suma1 = 0, suma2 = 0;
+	long int suma1 = 0;
+	long int suma2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
@@ -19,5 +19,5 @@ void print_diagsums(int *a, int size)
 		suma2 += a[i * size + (size - 1 - i)];
 	}
 
-	printf("%d, %d\n", suma1, suma2);
+	printf("%ld, %ld\n", suma1, suma2);
 }
