abc21c1fafd64bcf6c17ce3fd7863fe8b7697a10
p28Gamaliel <gamaliel13perez@gmail.com>
2025-07-14 16:19:50 +0000

    create_array

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *create_array - crea una matriz
+ * @size: tama√o que quiero
+ * @c: posicion de cada valor que recibira
+ * Return: la matriz
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	array = malloc(size * sizeof(char));
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		array[i] = c;
+
+	return (array);
+}
