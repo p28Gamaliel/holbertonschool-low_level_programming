bc18c2ce8c2e3dac84e27d336b07d45f2c86efbc
p28Gamaliel <gamaliel13perezgmail.com>
2025-07-14 01:32:28 +0000

    whatsmyname

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - imprime el nombre del programa
+ * @argc: numero de parametros
+ * @argv: array de argumentos
+ * Return: siempre 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
