909a13e7ba41b5c7ebd47319b0c3ebdfe8a61592
p28Gamaliel <gamaliel13perez@gmail.com>
2025-07-23 17:29:37 +0000

    print_list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - imprime los valores de una lista 
+ * @head: el puntero al siguiente nodo
+ *
+ * Return: el numero de nodos
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
