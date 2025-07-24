b837bf37a5bdb425f1a4b5ca7c5bc6b963c92664
p28Gamaliel <gamaliel13perez@gmail.com>
2025-07-24 17:49:10 +0000

    add_node

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,36 @@
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_node - anade el nuevo nodo como cabecera
+ *
+ * @str: la cadena
+ * @head: la cabecera
+ *
+ * Return: devuelve el nuevo nodo
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
