c41bde248bc9a61f015abdfe6d428aa7f6846584
p28Gamaliel <gamaliel13perez@gmail.com>
2025-07-24 18:13:57 +0000

    add_node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -18,19 +18,17 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 
 	new_node = malloc(sizeof(list_t));
-
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->str = strdup(str);
-
 	if (new_node->str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
 
-	new_node->len = strdup(str);
+	new_node->len = 0;
 	new_node->next = *head;
 	*head = new_node;
 
